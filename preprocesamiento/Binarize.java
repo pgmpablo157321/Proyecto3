babf975a1e41596220ac6eb088e2f246a3ec5f9d
DiegoOquendo <diego.oquendo1@gmail.com>
2018-07-27 14:05:24 -0500

    Binarize img

--- preprocesamiento/Binarize.java
+++ preprocesamiento/Binarize.java
@@ -7,57 +7,45 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Binarizate {
-	
-public static BufferedImage getImagen(int[] pixels){
-		
+public class Binarize{
+
+	public static BufferedImage getImagen(int[] pixels){
+
 		BufferedImage img=new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_BINARY);
-		 for(int i=0;i<img.getHeight();i++){
-			 for(int j=0;j<img.getWidth();j++){
-				 img.setRGB(j, i, pixels[i*2048+j]);
-			 }
-		 }
+		for(int i=0;i<img.getHeight();i++){
+			for(int j=0;j<img.getWidth();j++){
+				img.setRGB(j, i, pixels[i*2048+j]);
+			}
+		}
 		return img;
 	}
 
-public static int[] Initialize(BufferedImage a) {
-	int[]pixels=new int[a.getWidth()*a.getHeight()];
-	Color myWhite=new Color(0,0,0);
-	Color myBlack=new Color(255,255,255);
-	int rgb1=myWhite.getRGB();
-	int rgb2=myBlack.getRGB();
-	for(int i=0;i<a.getHeight();i++) {
-		for(int j=0; j<a.getWidth();j++) {
-			//Oquendo completar este if
-			if() {
-				//si es blanco
-				pixels[i*a.getWidth()+j]=rgb1;	
-			}else {
-				//si es negro
-				pixels[i*a.getWidth()+j]=rgb2;
-			}
-			
-		}
-	}
-	return pixels;
-}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*int []pixels=new int[768];
+	public static int[] Initialize(BufferedImage a) {
+		int[]pixels=new int[a.getWidth()*a.getHeight()];
 		Color myWhite=new Color(0,0,0);
 		Color myBlack=new Color(255,255,255);
-		int rgb1=myWhite.getRGB();
-		int rgb2=myBlack.getRGB();
-		File outputfile=new File("C:/Users/pgmpa/eclipse-workspace/Proyecto3/data/Imagenes/prueba1.jpg");
-	    try {
-			ImageIO.write(getImagen(pixels), "png", outputfile);
+		int rgbBlanco=myWhite.getRGB();
+		int rgbNegro=myBlack.getRGB();
+		for(int i=0;i<a.getHeight();i++) {
+			for(int j=0; j<a.getWidth();j++) {
+				Color pixel = new Color(a.getRGB(j,i));
+				if(pixel.getBlue()==255 && pixel.getRed()==255 && pixel.getGreen()==255) {
+					pixels[i*a.getWidth()+j]=rgbBlanco;
+				}else{
+					pixels[i*a.getWidth()+j]=rgbNegro;
+				}
+			}
+		}
+		return pixels;
+	}
+
+	public static void main(String[] args) {
+		File outputfile=new File("/home/oquendo/Desktop/Proyecto3/images/At_100x100_iconjake.jpg");
+		BufferedImage image = ImageIO.read(outputfile);
+		try {
+			int pixels[] = Binarize.Initialize(image);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
-		
-		
+		}
 	}
 }
